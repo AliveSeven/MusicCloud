6a1b0a1e915e75685e4f1378988aae284cd5a5aa
AliveSeven <2510867004@qq.com>
2022-09-05 23:08:59 +0800

    移动端UI优化

--- src/views/pc/04.mvs.vue
+++ src/views/pc/04.mvs.vue
@@ -1,153 +1,157 @@
 <template>
   <div class="mvs-container">
     <div class="filter-wrap">
-      <div class="section-wrap">
-        <!-- 分类切换地区 -->
-        <span class="section-type">地区:</span>
-        <!-- 标签包 -->
-        <ul class="tabs-wrap">
-          <!-- 标签 -->
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: area == '全部' }"
-              @click="area = '全部'"
-              >全部</span
-            >
-          </li>
+      <div class="wrap-container">
+          <!-- 地区包 -->
+        <div class="section-wrap">
+          <!-- 分类切换地区 -->
+          <span class="section-type">地区:</span>
+          <!-- 标签包 -->
+          <ul class="tabs-wrap">
+            <!-- 标签 -->
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: area == '全部' }"
+                @click="area = '全部'"
+                >全部</span
+              >
+            </li>
 
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: area == '内地' }"
-              @click="area = '内地'"
-              >内地</span
-            >
-          </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: area == '内地' }"
+                @click="area = '内地'"
+                >内地</span
+              >
+            </li>
 
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: area == '港台' }"
-              @click="area = '港台'"
-              >港台</span
-            >
-          </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: area == '港台' }"
+                @click="area = '港台'"
+                >港台</span
+              >
+            </li>
 
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: area == '欧美' }"
-              @click="area = '欧美'"
-              >欧美</span
-            >
-          </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: area == '欧美' }"
+                @click="area = '欧美'"
+                >欧美</span
+              >
+            </li>
 
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: area == '日本' }"
-              @click="area = '日本'"
-              >日本</span
-            >
-          </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: area == '日本' }"
+                @click="area = '日本'"
+                >日本</span
+              >
+            </li>
 
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: area == '韩国' }"
-              @click="area = '韩国'"
-              >韩国</span
-            >
-          </li>
-        </ul>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: area == '韩国' }"
+                @click="area = '韩国'"
+                >韩国</span
+              >
+            </li>
+          </ul>
+        </div>
+
+        <!-- 类型包 -->
+        <div class="type-wrap">
+          <span class="type-type">类型:</span>
+          <ul class="tabs-wrap">
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: type == '全部' }"
+                @click="type = '全部'"
+              >
+                全部
+              </span>
+            </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: type == '官方版' }"
+                @click="type = '官方版'"
+              >
+                官方版
+              </span>
+            </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: type == '原声' }"
+                @click="type = '原声'"
+              >
+                原声
+              </span>
+            </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: type == '现场版' }"
+                @click="type = '现场版'"
+              >
+                现场版
+              </span>
+            </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: type == '网易出品' }"
+                @click="type = '网易出品'"
+              >
+                网易出品
+              </span>
+            </li>
+          </ul>
+        </div>
+
+        <!-- 排序包 -->
+        <div class="order-wrap">
+          <span class="order-type">排序:</span>
+          <ul class="tabs-wrap">
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: order == '上升最快' }"
+                @click="order = '上升最快'"
+              >
+                上升最快
+              </span>
+            </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: order == '最热' }"
+                @click="order = '最热'"
+              >
+                最热
+              </span>
+            </li>
+            <li class="tab">
+              <span
+                class="title"
+                :class="{ active: order == '最新' }"
+                @click="order = '最新'"
+              >
+                最新
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
 
-      <!-- 类型包 -->
-      <div class="type-wrap">
-        <span class="type-type">类型:</span>
-        <ul class="tabs-wrap">
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: type == '全部' }"
-              @click="type = '全部'"
-            >
-              全部
-            </span>
-          </li>
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: type == '官方版' }"
-              @click="type = '官方版'"
-            >
-              官方版
-            </span>
-          </li>
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: type == '原声' }"
-              @click="type = '原声'"
-            >
-              原声
-            </span>
-          </li>
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: type == '现场版' }"
-              @click="type = '现场版'"
-            >
-              现场版
-            </span>
-          </li>
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: type == '网易出品' }"
-              @click="type = '网易出品'"
-            >
-              网易出品
-            </span>
-          </li>
-        </ul>
-      </div>
-
-      <!-- 排序包 -->
-      <div class="order-wrap">
-        <span class="order-type">排序:</span>
-        <ul class="tabs-wrap">
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: order == '上升最快' }"
-              @click="order = '上升最快'"
-            >
-              上升最快
-            </span>
-          </li>
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: order == '最热' }"
-              @click="order = '最热'"
-            >
-              最热
-            </span>
-          </li>
-          <li class="tab">
-            <span
-              class="title"
-              :class="{ active: order == '最新' }"
-              @click="order = '最新'"
-            >
-              最新
-            </span>
-          </li>
-        </ul>
-      </div>
     </div>
     <!-- mv容器 -->
     <!-- 推荐MV -->
